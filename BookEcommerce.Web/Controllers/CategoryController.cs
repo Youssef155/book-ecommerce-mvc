4ef101bd8192c1d0df3122362a9933713685d6c8
Youssef George <yousifkhaleel00@gmail.com>
2025-03-31 19:31:19 +0200

    create category

---
+++ BookEcommerce.Web/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BookEcommerce.Web.Data;
+using BookEcommerce.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookEcommerce.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> catList = _context.Categories.ToList();
+            return View(catList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- BookEcommerce.Web/Data/ApplicationDbContext.cs
+++ BookEcommerce.Web/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookEcommerce.Web.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookEcommerce.Web.Data
 {
@@ -6,7 +7,17 @@ namespace BookEcommerce.Web.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-             
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name= "Action", DisplayOrder = 1},
+                new Category { Id = 2, Name= "SciFi", DisplayOrder = 2},
+                new Category { Id = 3, Name= "History", DisplayOrder = 3}
+                );
         }
     }
 }
