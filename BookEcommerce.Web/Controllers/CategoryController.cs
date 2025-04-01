51c55086e3a94397fd07c095a3a099bcb7c84a4a
Youssef George <yousifkhaleel00@gmail.com>
2025-04-01 11:35:21 +0200

    finish category functionality

--- BookEcommerce.Web/Controllers/CategoryController.cs
+++ BookEcommerce.Web/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BookEcommerce.Web.Data;
 using BookEcommerce.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookEcommerce.Web.Controllers
 {
@@ -21,6 +22,68 @@ namespace BookEcommerce.Web.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category is created successfully";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null || id == 0)
+                return NotFound();
+
+            Category? category = _context.Categories.Find(id);
+
+            if (category is null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category is updated successfully";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id is null || id == 0)
+                return NotFound();
+
+            Category? category = _context.Categories.Find(id);
+
+            if (category is null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            if (id is null || id == 0)
+                return NotFound();
+
+            Category? category = _context.Categories.Find(id);
+
+            if (category is null)
+                return NotFound();
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category is deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
