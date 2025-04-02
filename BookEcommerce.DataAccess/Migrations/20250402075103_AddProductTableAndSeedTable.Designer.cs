e007e39e1f88da74f8e9af29ad34d50066fac0e6
Youssef George <yousifkhaleel00@gmail.com>
2025-04-02 09:52:49 +0200

    add areas and add product table

--- BookEcommerce.DataAccess/Migrations/20250402075103_AddProductTableAndSeedTable.Designer.cs
+++ BookEcommerce.DataAccess/Migrations/20250402075103_AddProductTableAndSeedTable.Designer.cs
@@ -3,16 +3,19 @@ using BookEcommerce.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace BookEcommerce.DataAccess.Migrations
+namespace BookEcommerce.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250402075103_AddProductTableAndSeedTable")]
+    partial class AddProductTableAndSeedTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
