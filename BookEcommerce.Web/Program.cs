8ddc38fd2c5dce20967ab332849fda992e635f87
Youssef George <yousifkhaleel00@gmail.com>
2025-04-03 14:52:47 +0200

    add identity package

--- BookEcommerce.Web/Program.cs
+++ BookEcommerce.Web/Program.cs
@@ -2,6 +2,7 @@ using BookEcommerce.DataAccess.Data;
 using BookEcommerce.DataAccess.Repository;
 using BookEcommerce.DataAccess.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,11 @@ var conString = builder.Configuration.GetConnectionString("DefaultConnection") ?
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(conString));
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => 
+     options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
@@ -31,8 +37,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
