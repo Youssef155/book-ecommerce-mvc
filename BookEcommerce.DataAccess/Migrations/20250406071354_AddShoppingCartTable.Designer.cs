71aa0644b5d00ecf776afb9dc0ab93f7f6cb0762
Youssef George <yousifkhaleel00@gmail.com>
2025-04-06 09:14:42 +0200

    add shoping cart class to the database

--- BookEcommerce.DataAccess/Migrations/20250406071354_AddShoppingCartTable.Designer.cs
+++ BookEcommerce.DataAccess/Migrations/20250406071354_AddShoppingCartTable.Designer.cs
@@ -4,16 +4,19 @@ using BookEcommerce.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace BookEcommerce.DataAccess.Migrations
+namespace BookEcommerce.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250406071354_AddShoppingCartTable")]
+    partial class AddShoppingCartTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookEcommerce.Models/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookEcommerce.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Please a value between 1 and 1000")]
+        public int Count { get; set; }
+        public int ProductId { get; set; }
+
+        [ValidateNever]
+        public Product Product { get; set; }
+        public string ApplicationUserId { get; set; }
+
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+
+    }
+}
