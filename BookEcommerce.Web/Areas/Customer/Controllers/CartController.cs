25ca41c5551dbfbb2e73f8543e28ecdee219e609
Youssef George <yousifkhaleel00@gmail.com>
2025-04-07 13:47:47 +0200

    finish shopping cart UI

--- BookEcommerce.Web/Areas/Customer/Controllers/CartController.cs
+++ BookEcommerce.Web/Areas/Customer/Controllers/CartController.cs
@@ -36,11 +36,50 @@ namespace BookEcommerce.Web.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(sc => sc.Id == cartId);
+            cartFromDb.Count++;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(sc => sc.Id == cartId);
+            if (cartFromDb.Count <= 1)
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            else
+            {
+                cartFromDb.Count--;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(sc => sc.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
         public double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
         {
-           if(shoppingCart.Count <= 50)
+            if (shoppingCart.Count <= 50)
                 return shoppingCart.Product.Price;
-           else
+            else
             {
                 if (shoppingCart.Count <= 100)
                     return shoppingCart.Product.Price50;
